7928767356c440b0a7fd9781c991c127951b8f9c
Tim Hughes <thughes@thegoldfish.org>
2022-12-07 17:17:06 +0000

    Update day_01.py

--- 2022/day_01/day_01.py
+++ 2022/day_01/day_01.py
@@ -5,8 +5,8 @@ with open("./input_day01") as fh:
     elves = [sum(map(int, elf_data.splitlines())) for elf_data in fh.read().split('\n\n')]
    
         
-q1 = "- Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?"
+q1 = "Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?"
 print("%s: %s" % (q1, max(elves)))
 
-q2 = "- Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?"
+q2 = "Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?"
 print("%s: %s" % (q2, sum(sorted(elves, reverse=True)[:3])))
